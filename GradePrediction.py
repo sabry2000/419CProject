2b835f9ebdbd4895d322afc5476c6953992e2f10
Callumari <ctakasaka@outlook.com>
2020-03-26 15:30:03 -0700

    Grade Prediction & Collaborative Filtering Stuff

---
+++ GradePrediction.py
@@ -0,0 +1,46 @@
+import csv
+import numpy as np
+from numpy.linalg import norm
+
+grades = []
+
+with open('samples.csv','r') as sampleGrades:
+    gradeReader = csv.reader(sampleGrades)
+    for student in gradeReader:
+        grades.append(student)
+
+# turning data into a numpy array
+grades = np.array(([i for i in grades if 'class1' not in i]), dtype=float)
+numStudents = grades.shape[0]
+# initializing cosine similarity matrix
+cos = [[0 for i in range(numStudents)] for j in range(numStudents)]
+for i in range(numStudents):
+    for j in range(numStudents):
+        # just set to 1 if it's on the diagonal of cos matrix
+        if i is j:
+            cos[i][j] = 1
+        else:
+            # create array with only the 2 students
+            comparison = np.array((grades[i],grades[j]))
+            # remove a course if 1 of the students hasn't taken it
+            comparison = comparison[:, np.all(comparison != 0, axis=0)]
+            # get similarity of student i and j
+            cos[i][j] = np.dot(comparison[0],comparison[1]) / (norm(comparison[0]) * norm(comparison[1]))
+
+for i in cos:
+    print(i)
+
+sims = int(input("How many similar people do you want to include? (Up to %i) " % (numStudents-1)))
+stu = int(input("Who do you want to look at? "))
+mark = int(input("What class mark do you want to predict? "))
+
+# get 'stu' most similar students (ignoring themselves)
+homies = dict(zip([i for i in range(numStudents)],cos[stu]))
+homies = list({k: v for k, v in sorted(homies.items(), key=lambda item: item[1], reverse=True)}.keys())[1:sims+1]
+
+predGrade = np.mean(grades[stu][grades[stu] != 0])
+
+for i in homies:
+    predGrade += (grades[i][mark]-np.mean(grades[i][grades[i] !=0])) / len(homies)
+
+print(predGrade)
