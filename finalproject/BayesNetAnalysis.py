be97706f1bfd421b70777b8827021c8642f9d271
Ahmed Sabry <52863189+sabry2000@users.noreply.github.com>
2020-04-11 09:56:37 -0700

    analysis with test case

--- finalproject/BayesNetAnalysis.py
+++ finalproject/BayesNetAnalysis.py
@@ -9,10 +9,18 @@ def performAnalysis(student):
     numberofcourses = helper.getNumberOfCourses(grades)
     majorAverages = helper.getMajorsAverages(grades)
 
+    print(grades)
+    print(missingGrades)
+    print(numberofcourses)
+    print(majorAverages)
+
     for i,major in enumerate(config.majors):
-        bn = BayesNet.generateBayesianNetwork(missingGrades[i], majorAverages[i], student.interests[i], 'x', config.upperyearcourses[i])
+        grade = list(missingGrades[i])[1]
+        bn = BayesNet.generateBayesianNetwork(grade, majorAverages[i], int(student.interests[i]), 'x', config.upperyearcourses[i])
         finalcoursegradeprediction = bn.predict_proba({'Predicted Course Grade': 'pass class',
         'Number Of Major Courses': str(numberofcourses[i]),
         'Major Average': 'pass major',
         'Interests': 'Interested'})
+        print(finalcoursegradeprediction)
 
+performAnalysis(config.lower_year_students[0])
